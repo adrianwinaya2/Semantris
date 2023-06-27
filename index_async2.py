cfaced31e6af4b2d530a9e876d5ac1179902754c
adrianwinaya2 <67622373+adrianwinaya2@users.noreply.github.com>
2023-06-27 18:15:39 +0700

    animation

--- index_async2.py
+++ index_async2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, send_file
 import json
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -28,37 +28,47 @@ def embed(input):
 
 def similarity(target, word):
     vectors = model([target, word])
-    vector1 = vectors[0]
-    vector2 = vectors[1]
-    vector1 = vector1.reshape(1, -1)
-    vector2 = vector2.reshape(1, -1)
+    vector1 = vectors[0].reshape(1, -1)
+    vector2 = vectors[1].reshape(1, -1)
+
     return cosine_similarity(vector1, vector2)[0][0]
 
 def fill_words(words, history, target):
     while len(words) < max_words:
         index = rand.randint(1, corpus.shape[0] - 1)
-        word = corpus['common_noun'][index]
-        if word.lower() not in [w.lower() for w in history]:
+        word = corpus['common_noun'][index].lower()
+        if word not in [w.lower() for w in history]:
             words.insert(0, word)
             history.append(word)
     target = rand.choice(words[:-pop_boundary])
     return words, history, target
 
+# ! OTHERS
 @app.before_request
 def remove_error():
-    session['error'] = None
+    session['error'] = ''
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/gameover')
+def gameOver():
+    return render_template('gameover_jup.html')
+
+# Route to serve the video file
+@app.route('/background.mp4')
+def send_video():
+    return send_file('templates/background.mp4', mimetype='video/mp4')
+
+# ! ROUTING
 @app.route('/play', methods=['POST'])
 def play():
     session['score'] = 0
     session['target'] = ''
     session['words'] = []
     session['history'] = []
-    session['error'] = None
+    session['error'] = ''
 
     session['words'], session['history'], session['target'] = fill_words(session['words'], session['history'], session['target'])
 
@@ -70,38 +80,45 @@ def check():
     # Get the answer from the form data
     answer = str(request.form['answer']).lower()
     
-    if answer in (word.lower() for word in session['words']):
-        return jsonify({
-            'score': session['score'],
-            'words': session['words'],
-            'target': session['target'],
-            'error': 'Word already exists!'
-        })
-    
-    else:
-        print("Unsorted : ", session['words'])
+    if answer not in [word.lower() for word in session['words']]:
         words_dict = {w: similarity(answer, w) for w in session['words']}
         sorted_dict = dict(sorted(words_dict.items(), key=lambda x: x[1]))
         sorted_arr = list(sorted_dict.keys())
         sorted_list = sorted_arr.copy()
-        print(f"Sorted : ${sorted_arr}")
+
+        print("Unsorted : ", session['words'])
+        print(f"Sorted : {sorted_arr}")
 
         # If the target is top 4
         target_index = sorted_arr.index(session['target'])
         rank = len(sorted_arr) - target_index
-        print(rank)
 
         if rank <= pop_boundary:
             del sorted_arr[-pop_boundary:target_index+1]
             session['score'] += (5 - rank)
             session['words'], session['history'], session['target'] = fill_words(sorted_arr, session['history'], session['target'])
+            status = 'success'
+        else:
+            session['words'] = sorted_list
+            status = 'fail'
         
         return jsonify({
             'score': session['score'],
             'words': session['words'],
             'sorted': sorted_list,
-            'target': session['target']
+            'target': session['target'],
+            'error': '',
+            'status': status
         })
+    
+    return jsonify({
+        'score': session['score'],
+        'words': session['words'],
+        'sorted': [],
+        'target': session['target'],
+        'error': 'Word already exists!',
+        'status': ''
+    })
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
